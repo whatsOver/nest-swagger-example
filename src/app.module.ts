dabcb2c54755945906c748805a88117ec62dd25b
ww8007 <wshmin1234@gmail.com>
2023-06-30 02:27:29 +0900

    feat: 각 도메인들에 대한 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { BlogModule } from './blog/blog.module';
+import { DeveloperModule } from './developer/developer.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, BlogModule, DeveloperModule],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
