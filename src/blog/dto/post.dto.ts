e1e16424ce6e3cc83acbeffe4be3dec9eaac96b0
ww8007 <wshmin1234@gmail.com>
2023-07-01 17:54:04 +0900

    feat: PostDto, DeletePostDto 추가

--- src/blog/dto/post.dto.ts
+++ src/blog/dto/post.dto.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+export class PostDto {
+  @ApiProperty({
+    example: '041c8e17-bdb5-4a4a-8c46-873b680bb8f8',
+    description: '게시글 아이디',
+  })
+  postId!: string;
+}
+
 export class UpdatePostDto {
   @ApiProperty({
     description: 'postId',
@@ -18,4 +26,12 @@ export class UpdatePostDto {
     example: '내용',
   })
   content!: string;
+}
+
+export class DeletePostDto {
+  @ApiProperty({
+    description: 'postId',
+    example: 'b9a8f8f5-a5e1-4648-94a2-a3f64d0027a9',
+  })
+  postId!: string;
 }
